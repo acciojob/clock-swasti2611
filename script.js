22faa169e51fdf3e6ae660783711a28fdcfc0568
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-01-20 22:39:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let time=document.getElementById("timer");
+
+setInterval(function() 
+			{
+				let date=new Date();
+				time.innerHTML =date.toLocaleString();
+			}, 1000);
